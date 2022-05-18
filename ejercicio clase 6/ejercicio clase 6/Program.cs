12c9ae2809eaa77485bd487e7c323894d6831262
Lobotx <104751530+Lobotx@users.noreply.github.com>
2022-05-18 18:03:43 -0300

    Add files via upload

---
+++ ejercicio clase 6/ejercicio clase 6/Program.cs
@@ -0,0 +1,41 @@
+﻿var adivinarNumero = true;
+// Generar numero secreto al azar
+var numeroDeIntentos = 0;
+
+
+int numeroSecreto = new
+        Random(DateTime.Now.Millisecond).Next(1, 21);
+
+Console.WriteLine("¡Bienvenido!");
+
+while (adivinarNumero)
+{
+    Console.WriteLine("Pulse una tecla para continuar...");
+    Console.ReadKey();
+    Console.Clear();
+    Console.WriteLine("Se ha generado un numero secreto...\n"+
+        "Debe adivinar el numero secreto elegido por skynet para derrotarlos\n"+
+       "Ingrese un numero: ");
+
+    numeroDeIntentos++;
+
+    var numeroDelUsuario = int.Parse(Console.ReadLine());
+
+        if (numeroDelUsuario < numeroSecreto)
+        {
+            Console.WriteLine("El numero que tiraste esta chiquito");
+            
+
+        }
+        else if (numeroDelUsuario > numeroSecreto)
+        {
+            Console.WriteLine("El numero que tiraste esta grandecito");
+        }
+        else
+        {
+            Console.WriteLine($"Bien ahi pa' Le ganaste a la maquina \nEl numero secreto era {numeroSecreto}");
+            Console.WriteLine($"Lo lograste en {numeroDeIntentos} intentos");
+            break;
+        }
+}
+
