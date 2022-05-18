a1b6fddc4e44f45a5fee52bbbabb7e7f1eee7a86
Lobotx <104751530+Lobotx@users.noreply.github.com>
2022-05-17 22:03:05 -0300

    Add files via upload

---
+++ ejercicio clase 5/ejercicio clase 5/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var consultaDeNombre = true;
+
+while (consultaDeNombre) 
+
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre por favor: ");
+    var nombre = Console.ReadLine();
+    Console.WriteLine($"hola {nombre}!");
+
+    Console.WriteLine("¿Desea continuar? S/N:");
+    var respuesta = Console.ReadLine();
+    if (respuesta.ToUpper() == "S")
+        consultaDeNombre = true; 
+    else if (respuesta.ToUpper() == "N")
+    {
+        Console.WriteLine("El programam finalizo correctamente");
+        consultaDeNombre = false;
+    }
+
+
+    else
+    {
+        Console.WriteLine("Opcion no valida");
+        consultaDeNombre = false;
+    }   
+
+}
+
+
+Console.ReadKey();
