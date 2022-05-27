f24a5eb0daa4403de29bd4964e4ada310700d5f1
Lobotx <104751530+Lobotx@users.noreply.github.com>
2022-05-27 04:52:03 -0300

    Add files via upload

---
+++ Clase 9/Ejercicio numeros santiagueños/Ejercicio numeros santiagueños/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Cuantos santiagueños quiere imprimir?:");
+int n = int.Parse(Console.ReadLine());
+int[] primos = new int[n];
+//para lista
+List<int> primos2 = new List<int>(); //<> comillas angulares
+
+int posicion = 0;
+int aux = 0;
+int numero = 1;
+while (posicion < n) 
+{
+    aux = 0;
+    for (var i = 1; i <= numero; i++)
+    {
+        if (numero % i == 0)
+        {
+            aux++;
+        }
+           
+        
+    }
+    if (aux == 2)
+    {
+        primos[posicion] = numero;
+        posicion++;
+    }
+    numero++;
+}
+foreach (var i in primos)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadKey();
