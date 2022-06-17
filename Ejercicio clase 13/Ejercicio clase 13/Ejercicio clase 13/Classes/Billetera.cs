ce9c1290cae4828d8d1023c929b412ab3542b743
Lobotx <sitios.joel2@hotmail.com>
2022-06-16 21:49:11 -0300

    Ejercicio agregado

---
+++ Ejercicio clase 13/Ejercicio clase 13/Ejercicio clase 13/Classes/Billetera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_clase_13.Classes
+{
+    internal class Billetera
+    {
+        private int _billetesDe10;
+        private int _billetesDe20;
+        private int _billetesDe50;
+        private int _billetesDe100;
+        private int _billetesDe200;
+        private int _billetesDe500;
+        private int _billetesDe1000;
+        public int BilletesDe10 { get { return _billetesDe10; } set { _billetesDe10 = value; } }
+        public int BilletesDe20 { get { return _billetesDe20; } set { _billetesDe20 = value; } }
+        public int BilletesDe50 { get { return _billetesDe50; } set { _billetesDe50 = value; } }
+        public int BilletesDe100 { get { return _billetesDe100; } set { _billetesDe100 = value; } }
+        public int BilletesDe200 { get { return _billetesDe200; } set { _billetesDe200 = value; } }
+        public int BilletesDe500 { get { return _billetesDe500; } set { _billetesDe500 = value; } }
+        public int BilletesDe1000 { get { return _billetesDe1000; } set { _billetesDe1000 = value; } }
+
+
+        public decimal Total()
+        {
+
+            decimal DineroTotal = BilletesDe10 * 10 + BilletesDe20 * 20 + BilletesDe50 * 50
+                + BilletesDe100 * 100 + BilletesDe200 * 200 + BilletesDe500 * 500 + BilletesDe1000 * 1000;
+
+            return DineroTotal;
+        }
+        //public Billetera BilleterasCombinadas()
+        //{
+        //    return new Billetera
+        //}
+
+
+        public Billetera BilleteraCombinada;
+
+        public Billetera Combinar(Billetera billetera1, Billetera billetera2)
+        {
+            Billetera billetera = new Billetera();
+            billetera.BilletesDe10 = billetera1.BilletesDe10 + billetera2.BilletesDe10;
+            billetera.BilletesDe20 = billetera1.BilletesDe20 + billetera2.BilletesDe20;
+            billetera.BilletesDe50 = billetera1.BilletesDe50 + billetera2.BilletesDe50;
+            billetera.BilletesDe100 = billetera1.BilletesDe100 + billetera2.BilletesDe100;
+            billetera.BilletesDe200 = billetera1.BilletesDe200 + billetera2.BilletesDe200;
+            billetera.BilletesDe500 = billetera1.BilletesDe500 + billetera2.BilletesDe500;
+            billetera.BilletesDe1000 = billetera1.BilletesDe1000 + billetera2.BilletesDe1000;
+            billetera1.BilletesDe10 = 0;
+            billetera1.BilletesDe20 = 0;
+            billetera1.BilletesDe50 = 0;
+            billetera1.BilletesDe100 = 0;
+            billetera1.BilletesDe200 = 0;
+            billetera1.BilletesDe500 = 0;
+            billetera1.BilletesDe1000 = 0;
+            billetera2.BilletesDe10 = 0;
+            billetera2.BilletesDe20 = 0;
+            billetera2.BilletesDe50 = 0;
+            billetera2.BilletesDe100 = 0;
+            billetera2.BilletesDe200 = 0;
+            billetera2.BilletesDe500 = 0;
+            billetera2.BilletesDe1000 = 0;
+
+            return billetera;
+        }
+
+    }
+}
---
+++ Ejercicio clase 13/Ejercicio clase 13/Ejercicio clase 13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Ejercicio_clase_13.Classes;
+
+var Billetera1 = new Billetera() //La mia ni de casualidad
+{
+    BilletesDe10 = 5,
+    BilletesDe20 = 3,
+    BilletesDe50 = 1,
+    BilletesDe100 = 6,
+    BilletesDe200 = 2,
+    BilletesDe500 = 6,
+    BilletesDe1000 = 2,
+};
+
+var Billetera2 = new Billetera() 
+{
+    BilletesDe10 = 3,
+    BilletesDe20 = 6,
+    BilletesDe50 = 3,
+    BilletesDe100 = 2,
+    BilletesDe200 = 1,
+    BilletesDe500 = 3,
+    BilletesDe1000 = 1,
+};
+var Billetera3 = new Billetera();
+
+Console.WriteLine($"El importe total de la Primer billetera es {Billetera1.Total()}");
+Console.WriteLine($"El importe total de la Segunda billetera es {Billetera2.Total()}");
+
+
+
+Billetera3 = Billetera3.Combinar(Billetera1, Billetera2);
+
+
+Console.WriteLine($"La combinacion de ambas billeteras es {Billetera3.Total()}");
+Console.WriteLine($"El nuevo valor de la Primer billetera es {Billetera1.Total()}");
+Console.WriteLine($"El nuevo valor de la Primer billetera es {Billetera2.Total()}");
